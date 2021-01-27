e97842dffddedebf1bc4cc876b423205707676e0
Ashley Hutchinson <hutchski@Ashleys-MBP.fios-router.home>
2021-01-27 14:08:29 -0500

    for issue ticket

--- app.js
+++ app.js
@@ -1,29 +1,44 @@
 let userInput = document.querySelector('#search-field')
 let inputButton = document.querySelector('#search')
 
-
+// search function 
 async function parkResult() {
   let userSearch = userInput.value.toLowerCase()
   const url = `https://developer.nps.gov/api/v1/parks?q=${userSearch}&api_key=fxheInC78kFAXET9y492TAWBb706vN5yWmjyIfTb`
-
-  // console.log(`user search: `, userSearch)
 
   try {
     let response = await axios.get(url)
 
     let parksArray = response.data.data
+    // console.log(parksArray)
     for (let i = 0; i < parksArray.length; i++) {
-      let parkName = response.data.data[i].fullName.toLowerCase()
-      if (parkName.includes(userSearch)) {
-        console.log(`actual park:`, parkName)
+      let parkChosen = response.data.data[i].fullName.toLowerCase()
+      if (parkChosen.includes(userSearch)) {
+        // console.log(`actual park:`, parkChosen)
+        renderResults(parkChosen)
       }
     }
-
   } catch (error) {
     console.log(error)
   }
 
 }
+parkResult()
+
+// display the results
+function renderResults(parkChosen) {
+  let resultsContainer = document.querySelector('.results')
+
+  let displayPark = `
+  <h2 class='park-name'>${parkChosen}</h2>
+  <img src='${parkChosen.images[0]}' alt='park photo'>
+  `
+
+
+  resultsContainer.insertAdjacentHTML('afterbegin', displayPark)
+
+}
+
 
 inputButton.addEventListener('click', (e) => {
   e.preventDefault()
